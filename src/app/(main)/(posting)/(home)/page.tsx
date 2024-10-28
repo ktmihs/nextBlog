ec05fe90d307a1bd1cd2e8d76bee21fcfbcf536f
ktmihs <yellowgreen423@gmail.com>
2024-10-28 14:35:22 +0900

    homepage 레이아웃 수정 및 임시 article 추가
    - 임시 article 추가하여 영역 확인 및 스타일 추가
    - 추후 component 분리할 예정

--- src/app/(main)/(posting)/(home)/page.tsx
+++ src/app/(main)/(posting)/(home)/page.tsx
@@ -1,11 +1,48 @@
 import type { NextPage } from 'next';
 import styles from './page.module.css';
+import Link from 'next/link';
+
+interface articleType {
+	id: number;
+	title: string;
+	thumbnail: string;
+	summary: string;
+	url: string;
+}
 
 const Home: NextPage = () => {
+	// 임시 데이터 추가
+	const articleList = new Array(4).fill(null).reduce((list, _, idx) => {
+		const newArticle = {
+			id: 1000 + idx,
+			title: `article${idx + 1}`,
+			thumbnail: 'http://test.jpg',
+			summary: 'qwertyuiop asdfghjkl zxcvbnm...',
+			url: 'https://naver.com',
+		};
+		return [...list, newArticle];
+	}, []);
+
 	return (
-		<div className={styles.page}>
-			<div>main page</div>
-		</div>
+		<section className={styles.page}>
+			<section className={styles.listInfo}>
+				<span>total 1</span>
+				<div>dropdown</div>
+			</section>
+			<section className={styles.articleList}>
+				{articleList.map((article: articleType) => {
+					return (
+						<Link href={article.url} key={article.id}>
+							<article className={styles.article}>
+								<img src={article.thumbnail} alt={article.title} />
+								<span>{article.title}</span>
+								<span>{article.summary}</span>
+							</article>
+						</Link>
+					);
+				})}
+			</section>
+		</section>
 	);
 };
 
