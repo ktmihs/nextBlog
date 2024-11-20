ae518bc3245b7d346ac5b988dd6c2e137d899d25
ktmihs <yellowgreen423@gmail.com>
2024-11-20 14:28:26 +0900

    [feat] cateory reducer 생성 및 적용
    - 카테고리 관련 Reducer 생성
    - 공통 레이아웃에 적용 및 테스트
    - 기존의 key 속성은 id와 동일하게 사용될 것 같아 id 속성으로 통합
    - DB 연결 시, initialState는 api를 통해 받아올 예정

---
+++ src/lib/features/category/categorySlice.ts
@@ -0,0 +1,39 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface CategoryType {
+	id: string;
+	name: string;
+}
+
+interface CategoryStateType {
+	categoryList: CategoryType[];
+	selectedCategory: string | null;
+}
+
+const initialState: CategoryStateType = {
+	categoryList: [
+		{ id: 'all', name: '전체' },
+		{ id: 'project', name: 'project' },
+		{ id: 'study', name: 'study' },
+	],
+	selectedCategory: null,
+};
+
+const categorySlice = createSlice({
+	name: 'category',
+	initialState,
+	reducers: {
+		setCategory: (state, action: PayloadAction<CategoryType>) => {
+			state.categoryList.push({
+				id: action.payload.id,
+				name: action.payload.name,
+			});
+		},
+		selectCategory: (state, action: PayloadAction<string>) => {
+			state.selectedCategory = action.payload;
+		},
+	},
+});
+
+export const { setCategory, selectCategory } = categorySlice.actions;
+export default categorySlice.reducer;
--- src/lib/reducers.ts
+++ src/lib/reducers.ts
@@ -1,5 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit';
+import categoryReducer from '@lib/features/category/categorySlice';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+	category: categoryReducer,
+});
 
 export default rootReducer;
