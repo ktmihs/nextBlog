84f6daafdd598f0f1c0cd88cc8e1c1c1ef1591a8
ktmihs <yellowgreen423@gmail.com>
2024-11-21 10:53:38 +0900

    [design] category 페이지 임시 리스트 추가 및 기본 레이아웃 작업
    - 메인 페이지의 레이아웃과 유사하여 해당 부분은 나중에 공통 css로 관리할까 고민 중
    - 임시 데이터는 API 구현 이후 DB로부터 선택된 카테고리의 리스트를 받아와 store에서 관리할 때, store의 데이터를 가져와서 사용할 예정

--- src/app/(main)/(posting)/category/[id]/page.tsx
+++ src/app/(main)/(posting)/category/[id]/page.tsx
@@ -1,8 +1,53 @@
 import { FC } from 'react';
+import Link from 'next/link';
 import { IDPropsType } from '@/common/type';
+import styles from './page.module.css';
 
 const CategoryPage: FC<IDPropsType> = ({ params }) => {
-	return <div>category page ({params.id})</div>;
+	// 스토어에서 가져올 임시 데이터
+	const articleList = [
+		{
+			id: '1001',
+			title: 'title',
+			thumbnail: '/',
+			summary: '어떤 글이냐면 나도 모르겠는데 일단 그냥 고고',
+		},
+		{
+			id: '1002',
+			title: 'title',
+			date: '2024-11-21',
+			thumbnail: '/',
+			summary: '어떤 글이냐면 나도 모르겠는데 일단 그냥 고고',
+		},
+		{
+			id: '1003',
+			title: 'title',
+			date: '2024-11-21',
+			thumbnail: '/',
+			summary: '어떤 글이냐면 나도 모르겠는데 일단 그냥 고고',
+		},
+	];
+
+	return (
+		<>
+			<section className={styles.categoryList}>
+				<h2 className={styles.categoryName}>category page ({params.id})</h2>
+				<ul className={styles.articleList}>
+					{articleList.map(article => (
+						<li key={article.id} className={styles.articleItem}>
+							<article>
+								<Link href={`/detail/${article.id}`}>
+									<img src={article.thumbnail} className={styles.thumbnail} />
+									<h3 className={styles.title}>{article.title}</h3>
+									<span className={styles.summary}>{article.summary}</span>
+								</Link>
+							</article>
+						</li>
+					))}
+				</ul>
+			</section>
+		</>
+	);
 };
 
 export default CategoryPage;
