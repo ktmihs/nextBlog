b67813df7a57096fd04698263fd98b0948cb9ea0
ktmihs <yellowgreen423@gmail.com>
2024-12-04 14:34:08 +0900

    [feat] 상세글 페이지 articleDetail 컴포넌트 적용
    - 상세글 페이지에 articleDetail 컴포넌트 및 mock 데이터 적용

--- src/app/(main)/(posting)/detail/[id]/page.tsx
+++ src/app/(main)/(posting)/detail/[id]/page.tsx
@@ -1,22 +1,14 @@
 import { FC } from 'react';
 import { IDPropsType } from '@/common/type';
-import styles from './page.module.css';
+import styles from '@/app/(main)/(posting)/detail/[id]/page.module.css';
 import { articleList } from '@/common/component/article/article.mock';
 import { articleType } from '@/common/component/article/articleType';
-
 import ArticleRow from '@/common/component/article/articleRow';
+import ArticleDetail from '@/common/component/article/articleDetail';
 
 const DetailPage: FC<IDPropsType> = ({ params }) => {
 	// id 기반 페이지 렌더링
 	const { id } = params;
-	// store의 현재 카테고리를 통한 articleList API로 가져오기
-	const article = {
-		id: '1001',
-		title: '오늘의 글 제목',
-		author: 'ktmihs',
-		date: '2024-11-28',
-		content: '글글글글글',
-	};
 
 	return (
 		<div className={styles.articleContainer}>
@@ -31,15 +23,7 @@ const DetailPage: FC<IDPropsType> = ({ params }) => {
 				</div>
 			</section>
 			<hr />
-			<article>
-				<h2 className={styles.articleTitle}>{article.title}</h2>
-				<section className={styles.articleInfo}>
-					<span>{article.author}</span>
-					<span>•</span>
-					<span>{article.date}</span>
-				</section>
-				<section>{article.content}</section>
-			</article>
+			<ArticleDetail currentId={id} />
 		</div>
 	);
 };
