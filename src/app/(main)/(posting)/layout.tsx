ae859ad611b79cb196e0ff16334324920e487b6a
ktmihs <yellowgreen423@gmail.com>
2024-12-04 16:48:20 +0900

    [feat] 사이드바 ArticlePreviewList 컴포넌트 적용
    - 각 컴포넌트에 currentTab을 파라미터로 받아 상황에 맞는 데이터를 렌더링 할 수 있도록 적용

--- src/app/(main)/(posting)/layout.tsx
+++ src/app/(main)/(posting)/layout.tsx
@@ -1,41 +1,14 @@
 'use client';
 
 import React, { useState, useEffect, MouseEvent } from 'react';
-import Link from 'next/link';
 import style from '@app/(main)/(posting)/layout.module.css';
-import { useSelector } from 'react-redux';
-import { RootState } from '@lib/store';
+import ArticlePreviewList from '@/common/component/sidebar/articlePreviewList';
 
 export default function PostingLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-	interface categoryLenType {
-		[key: string]: number;
-	}
-
-	const categoryLen: categoryLenType = {
-		all: 20,
-		project: 13,
-		study: 7,
-	};
-
-	const currentPostList = [
-		{ id: '1', name: '가장 최근 글', thumbnail: '/public/img/test.jpg' },
-		{ id: '2', name: '두 번째 최근 글', thumbnail: '/public/img/test.jpg' },
-		{ id: '3', name: '세 번째 최근 글', thumbnail: '/public/img/test.jpg' },
-	];
-
-	const popularPostList = [
-		{ id: '1', name: '가장 인기 글', thumbnail: '/public/img/test.jpg' },
-		{ id: '2', name: '두 번째 인기 글', thumbnail: '/public/img/test.jpg' },
-		{ id: '3', name: '세 번째 인기 글', thumbnail: '/public/img/test.jpg' },
-	];
-
-	const categoryList = useSelector(
-		(state: RootState) => state.category.categoryList,
-	);
 	const [windowWidth, setWindowWidth] = useState<number>(0);
 	const [currentTab, setCurrentTab] = useState<string>('classifyPost');
 
@@ -79,56 +52,13 @@ export default function PostingLayout({
 					</section>
 				)}
 				{(windowWidth > 992 || currentTab === 'classifyPost') && (
-					<section className={style.classify}>
-						<b>분류</b>
-						<div>
-							{categoryList.map(category => {
-								return (
-									<Link href={`/category/${category.id}`} key={category.id}>
-										<section>
-											<p>
-												{category.name} ({categoryLen[category.id]})
-											</p>
-										</section>
-									</Link>
-								);
-							})}
-						</div>
-					</section>
+					<ArticlePreviewList currentTab="classifyPost" />
 				)}
 				{(windowWidth > 992 || currentTab === 'currentPost') && (
-					<section className={style.classify}>
-						<b>최근글</b>
-						<div>
-							{currentPostList.map(currentPost => {
-								return (
-									<Link href={`/detail/${currentPost.id}`} key={currentPost.id}>
-										<section className={style.summarySection}>
-											<p>{currentPost.name}</p>
-											<img src={currentPost.thumbnail} alt={currentPost.name} />
-										</section>
-									</Link>
-								);
-							})}
-						</div>
-					</section>
+					<ArticlePreviewList currentTab="currentPost" />
 				)}
 				{(windowWidth > 992 || currentTab === 'popularPost') && (
-					<section className={style.classify}>
-						<b>인기글</b>
-						<div>
-							{popularPostList.map(popularPost => {
-								return (
-									<Link href={`/detail/${popularPost.id}`} key={popularPost.id}>
-										<section className={style.summarySection}>
-											<p>{popularPost.name}</p>
-											<img src={popularPost.thumbnail} alt={popularPost.name} />
-										</section>
-									</Link>
-								);
-							})}
-						</div>
-					</section>
+					<ArticlePreviewList currentTab="popularPost" />
 				)}
 			</section>
 			<section className={style.mainContent}>{children}</section>
