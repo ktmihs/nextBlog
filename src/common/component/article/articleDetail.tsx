d3d79fdc33fcab415defc175ebc313e781d9b7cc
ktmihs <yellowgreen423@gmail.com>
2024-12-04 14:33:32 +0900

    [feat] articleDetail 컴포넌트 분리
    - 상세글에서 보여질 글 부분을 articleDetail 컴포넌트로 분리
    - 스타일 함께 분리
    - 이후 id를 통해 api로 데이터 불러올 예정
    - content 영역 따로 분리할지 고민하기

---
+++ src/common/component/article/articleDetail.tsx
@@ -0,0 +1,20 @@
+import styles from '@/common/component/article/articleDetail.module.css';
+import { article } from '@/common/component/article/article.mock';
+
+const ArticleDetail = ({ currentId }: { currentId: string }) => {
+	// store의 현재 currentId를 통한 articleList API로 가져오기
+
+	return (
+		<article>
+			<h2 className={styles.articleTitle}>{article.title}</h2>
+			<section className={styles.articleInfo}>
+				<span>{article.author}</span>
+				<span>•</span>
+				<span>{article.date}</span>
+			</section>
+			<section>{article.content}</section>
+		</article>
+	);
+};
+
+export default ArticleDetail;
