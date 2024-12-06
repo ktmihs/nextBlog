20c88395b6f1588e700741681ebbfc951014f381
ktmihs <yellowgreen423@gmail.com>
2024-12-06 10:02:49 +0900

    [feat] ArticleListControls에 Dropdown 컴포넌트 적용
    - Dropdown 컴포넌트 적용 및 임시 데이터 추가

--- src/common/component/ArticleListControls/ArticleListControls.tsx
+++ src/common/component/ArticleListControls/ArticleListControls.tsx
@@ -1,13 +1,36 @@
+'use client';
+
 import styles from '@common/component/ArticleListControls/ArticleListControls.module.css';
+import Dropdown from '@/common/component/Dropdown/Dropdown';
 import { articleList } from '@/common/component/Article/Article.mock';
 
 const ArticleListControls = () => {
 	const totalCnt = articleList.length;
+	const filterOption = [
+		{
+			key: 'option1',
+			value: '인기순',
+		},
+		{
+			key: 'option2',
+			value: '최신순',
+		},
+		{
+			key: 'option3',
+			value: '오래된순',
+		},
+	];
+
+	const handleChange = (key: string) => {
+		if (key) {
+			// key에 대한 정렬 방식으로 데이터 새로 받아오기
+		}
+	};
 
 	return (
 		<section className={styles.listInfo}>
 			<span>전체 {totalCnt}개의 글</span>
-			<div>dropdown</div>
+			<Dropdown dataList={filterOption} onChange={handleChange} />
 		</section>
 	);
 };
