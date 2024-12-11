c62ee5178405e4c685845cbf7031ba8c2afca55a
ktmihs <yellowgreen423@gmail.com>
2024-12-11 11:31:16 +0900

    [feat] Sidebar 컴포넌트 분리
    - 반응형 디자인을 위해 innerWidth 및 useEffect를 사용하여 csr을 해야하는 사이드바를 별도의 컴포넌트로 분리

--- src/app/(main)/(posting)/layout.tsx
+++ src/app/(main)/(posting)/layout.tsx
@@ -1,66 +1,15 @@
-'use client';
-
-import React, { useState, useEffect, MouseEvent } from 'react';
+import React from 'react';
 import style from '@app/(main)/(posting)/layout.module.css';
-import ArticlePreviewList from '@/common/component/Sidebar/ArticlePreviewList';
+import Sidebar from '@/common/component/Sidebar/Sidebar';
 
 export default function PostingLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-	const [windowWidth, setWindowWidth] = useState<number>(0);
-	const [currentTab, setCurrentTab] = useState<string>('classifyPost');
-
-	useEffect(() => {
-		const handleResize = () => setWindowWidth(window.innerWidth);
-		if (!windowWidth) handleResize();
-		window.addEventListener('resize', handleResize);
-
-		return () => window.removeEventListener('resize', handleResize);
-	}, []);
-
-	const handleClassify = (e: MouseEvent) => {
-		const { id } = e.target as HTMLSpanElement;
-		if (!id) return;
-		setCurrentTab(id);
-	};
-
 	return (
 		<>
-			<section className={style.sidebar}>
-				{windowWidth <= 992 && (
-					<section className={style.classifyTitle} onClick={handleClassify}>
-						<span
-							id="classifyPost"
-							className={currentTab === 'classifyPost' ? style.activeTab : ''}
-						>
-							분류
-						</span>
-						<span
-							id="currentPost"
-							className={currentTab === 'currentPost' ? style.activeTab : ''}
-						>
-							최근글
-						</span>
-						<span
-							id="popularPost"
-							className={currentTab === 'popularPost' ? style.activeTab : ''}
-						>
-							인기글
-						</span>
-					</section>
-				)}
-				{(windowWidth > 992 || currentTab === 'classifyPost') && (
-					<ArticlePreviewList currentTab="classifyPost" />
-				)}
-				{(windowWidth > 992 || currentTab === 'currentPost') && (
-					<ArticlePreviewList currentTab="currentPost" />
-				)}
-				{(windowWidth > 992 || currentTab === 'popularPost') && (
-					<ArticlePreviewList currentTab="popularPost" />
-				)}
-			</section>
+			<Sidebar />
 			<section className={style.mainContent}>{children}</section>
 		</>
 	);
